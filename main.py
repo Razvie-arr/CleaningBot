dfca6a66b7de835e9c49d8d64d8260670a8696a3
razviearr <vladbyastinov@mail.ru>
2022-02-15 22:10:25 +0100

    бот готов

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 import config
 import telebot
 import schedule, time
+from datetime import datetime
 
 bot = telebot.TeleBot(config.token)
 
 
-# def send_message():
-#     bot.send_message(-508454043, "хм че не отправляет")
+def send_message():
+    bot.send_message(-508454043, get_message(get_week_day()))
+
+
+def get_message(week_day):
+    message = 'Сегодня ' + week_day + '-й день, ' + get_trash_cleaner_name(week_day) + ' вынеси мусор \n ' + get_mention(get_trash_cleaner_name(week_day))
+    return message
+
+
+def get_week_day():
+    return str(datetime.isoweekday(datetime.today()))
+
+
+def get_trash_cleaner_name(week_day):
+    return {
+        '1': 'Ваня',
+        '2': 'Артур Б',
+        '3': 'Влад',
+        '4': 'Артур О',
+        '5': 'Рол',
+        '6': 'Сеня',
+        '7': 'Артур М'
+    }.get(week_day, 'error')
+
+
+def get_mention(trash_cleaner_name):
+    return {
+        'Ваня': '@Falx1',
+        'Артур Б': '@pretty_snyder',
+        'Влад': '@Razviearr',
+        'Артур О': '@hovhannesyan',
+        'Рол': '@arakelian',
+        'Сеня': '',
+        'Артур М': '@everyonehatesarthur'
+    }.get(trash_cleaner_name, 'error')
 
 
 def main():
-    # schedule.every().day.at("21:17").do(send_message)
-    bot.send_message(-508454043, "хм че не отправляет")
-    # send_message()
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    schedule.every().day.at("18:00").do(send_message)
+    schedule.every().day.at("20:00").do(send_message)
+    schedule.every().day.at("21:00").do(send_message)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
