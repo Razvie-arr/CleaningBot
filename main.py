c03795fa0785a8225c686bfd63a8a2368899ef77
Razvie-arr <vladbyastinov@mail.ru>
2022-04-11 21:47:51 +0200

    Senya and Artur moved out of the apartment, Slava moved in

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import config
-import telebot
-import schedule, time
 from datetime import datetime
 
-bot = telebot.TeleBot(config.token)
+import secret
+import schedule
+import telebot
+import time
+
+bot = telebot.TeleBot(secret.token)
 
 
 def send_message():
-    bot.send_message(-508454043, get_message(get_week_day()))
+    bot.send_message(secret.group_id, get_message(get_week_day()))
 
 
 def get_message(week_day):
-    message = 'Сегодня ' + week_day + '-й день, ' + get_trash_cleaner_name(week_day) + ' вынеси мусор \n ' + get_mention(get_trash_cleaner_name(week_day))
+    message = 'Сегодня ' + week_day + '-й день, ' + get_trash_cleaner_name(week_day) + ' вынеси мусор \n ' + secret.get_mention(get_trash_cleaner_name(week_day))
     return message
 
 
@@ -24,29 +26,13 @@ def get_trash_cleaner_name(week_day):
         '1': 'Ваня',
         '2': 'Артур Б',
         '3': 'Влад',
-        '4': 'Артур О',
         '5': 'Рол',
-        '6': 'Сеня',
-        '7': 'Артур М'
+        '6': 'Слава',
     }.get(week_day, 'error')
 
 
-def get_mention(trash_cleaner_name):
-    return {
-        'Ваня': '@Falx1',
-        'Артур Б': '@pretty_snyder',
-        'Влад': '@Razviearr',
-        'Артур О': '@hovhannesyan',
-        'Рол': '@arakelian',
-        'Сеня': '',
-        'Артур М': '@everyonehatesarthur'
-    }.get(trash_cleaner_name, 'error')
-
-
 def main():
-    schedule.every().day.at("18:00").do(send_message)
     schedule.every().day.at("20:00").do(send_message)
-    schedule.every().day.at("21:00").do(send_message)
     while True:
         schedule.run_pending()
         time.sleep(1)
