fbf9af19bdb1abd036e97dda6a89896ee99398e7
Razvie-arr <vladbyastinov@mail.ru>
2022-04-11 21:59:53 +0200

    token and group_id by functions

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ import schedule
 import telebot
 import time
 
-bot = telebot.TeleBot(secret.token)
+bot = telebot.TeleBot(secret.get_token())
 
 
 def send_message():
-    bot.send_message(secret.group_id, get_message(get_week_day()))
+    bot.send_message(secret.get_group_id(), get_message(get_week_day()))
 
 
 def get_message(week_day):
-    message = 'Сегодня ' + week_day + '-й день, ' + get_trash_cleaner_name(week_day) + ' вынеси мусор \n ' + secret.get_mention(get_trash_cleaner_name(week_day))
+    message = 'Сегодня ' + \
+              week_day + '-й день, ' + \
+              get_trash_cleaner_name(week_day) + \
+              ' вынеси мусор \n ' + \
+              secret.get_mention(get_trash_cleaner_name(week_day))
     return message
 
 
